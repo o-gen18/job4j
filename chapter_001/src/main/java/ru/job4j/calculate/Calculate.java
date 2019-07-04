1d60b2957731f5874cb0dc91f0d9b378333f3671
o-gen18 <oggen18@gmail.com>
2019-07-04 13:49:12 +0100

    1.3. JavaDoc. Code convention[#151190]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,15 @@
 package ru.job4j.calculate;
+/** 
+ * Calculate.
+ *
+ * @author Oleg Generalov
+*/
 
 public class Calculate {
+/**
+ * Main.
+ * @param args - args
+*/
 	public static void main(String[] args) {
 		System.out.println("Hello, Job4j, this is my first program!");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for calculate task
+ *
+ * @author Oleg Generalov (oggen18@gmail.com)
+ * @version 1.0
+ * @since 4.7.19
+*/
+package ru.job4j.calculate;
