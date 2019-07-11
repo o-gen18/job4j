cbd3e8d2596b6c296ad26d4d0eddc757472feccc
o-gen18 <oggen18@gmail.com>
2019-07-11 15:41:09 +0100

    1.4. JUnit[#151191]

---
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.calculate;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test.
+ *
+ * @author Oleg Generalov (oggen18@gmail.com)
+ * @version 1.0
+ * @since 11.7.19
+ */
+
+public class CalculateTest {
+/**
+ * Test echo.
+ */
+@Test
+public void whenTakeNameThenThreeEchoPlusName() {
+	String input = "Oleg Generalov";
+	String expect = "Echo, echo, echo : Oleg Generalov";
+	Calculate calc = new Calculate();
+	String result = calc.echo(input);
+	assertThat(result, is(expect));
+}
+
+}
--- chapter_001/src/test/java/ru/job4j/calculate/package-info.java
+++ chapter_001/src/test/java/ru/job4j/calculate/package-info.java
@@ -1,8 +1,7 @@
 /**
- * Package for calculate task
- *
+ * Package for CalculateTest
  * @author Oleg Generalov (oggen18@gmail.com)
- * @version 2.0
+ * @version 1.0
  * @since 11.7.19
-*/
+ */
 package ru.job4j.calculate;
