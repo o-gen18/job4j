a4fc6bd5425586332e216dcec727ce2b4c108674
o-gen18 <oggen18@gmail.com>
2019-08-12 16:31:45 +0100

    максимум из 3 чисел(2)

--- chapter_001/src/main/java/ru/job4j/condition/MultiMax.java
+++ chapter_001/src/main/java/ru/job4j/condition/MultiMax.java
@@ -2,7 +2,9 @@ package ru.job4j.condition;
 
 public class MultiMax {
     public int max(int first,int second, int third) {
-        int result=first>second?first>third?first:third:second>third?second:third;
+        int result;
+        if (first>second) {result=first>third?first:third;}
+        else {result=second>third?second:third;}
         return result;
     }
 }
