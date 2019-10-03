6bb3b080847e7bbc2cea6cb40175ccbf24095984
Ol Gen <oggen18@gmail.com>
2019-10-03 20:56:53 +0300

    дописал тест на всю пирамиду

--- chapter_001/src/test/java/ru/job4j/loop/PaintTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/PaintTest.java
@@ -44,5 +44,21 @@ public class PaintTest {
                 )
         );
     }
+    @Test
+    public void whenTheWholePyramid(){
+        Paint paint=new Paint();
+        String rst=paint.pyramid(4);
+        System.out.println(rst);
+        assertThat(rst,
+                is(
+                        new StringJoiner(System.lineSeparator(), "",System.lineSeparator())
+                                .add("   ^   ")
+                                .add("  ^^^  ")
+                                .add(" ^^^^^ ")
+                                .add("^^^^^^^")
+                                .toString()
+                )
+        );
+    }
 
 }
