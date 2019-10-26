e11e4bf39454f90e1e7622d84e26368529d41975
Ol Gen <oggen18@gmail.com>
2019-10-26 18:28:26 +0300

    6.4. Слово начинается с ...[#151166](убрал else)

--- chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -2,13 +2,11 @@ package ru.job4j.array;
 
 public class ArrayChar {
     public static boolean startsWith(char[] word,char[] pref){
-        boolean result=false;
+        boolean result=true;
         for(int i=0;i<pref.length;i++){
             if(pref[i]!=word[i]){
                 result=false;
                 break;
-            }else{
-                result=true;
             }
         }
         return result;
