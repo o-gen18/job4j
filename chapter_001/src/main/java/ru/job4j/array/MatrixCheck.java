ea3074e10099ab863eeb08dd687f2e83b8d07b06
Ol Gen <oggen18@gmail.com>
2019-10-30 10:05:56 +0300

    6.7. Квадратный массив заполнен true или false по диагоналям.[#151168]

---
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -0,0 +1,54 @@
+package ru.job4j.array;
+
+public class MatrixCheck {
+    public static boolean isWin(char[][]board){
+        boolean result=true;
+        for (int row=0;row<board.length;row++){
+            for (int cell=0;cell<board.length;cell++){
+                char sign=board[row][cell];
+                System.out.print(sign);
+                for(int i=0;i<cell;i++)
+                if(sign!='X'){
+                    result = false;
+                    break;
+                }
+                }System.out.println();
+            }return result;
+
+        }
+
+
+
+    public static void main(String[]ars){
+        char[][] hasWinVertical = {
+                {'_','_','X','_','_'},
+                {'_','_','X','_','_'},
+                {'_','_','X','_','_'},
+                {'_','_','X','_','_'},
+                {'_','_','X','_','_'}
+        };
+        boolean win=isWin(hasWinVertical);
+        System.out.println("A board has a winner: "+win);
+        System.out.println();
+        char[][]hasWinHor={
+                {'_','_','_','_','_'},
+                {'X','X','X','X','X'},
+                {'_','_','_','_','_'},
+                {'_','_','_','_','_'},
+                {'_','_','_','_','_'}
+        };
+        boolean winHor=isWin(hasWinHor);
+        System.out.println("A board has a winner: "+winHor);
+        System.out.println();
+        char[][]notWin={
+                {'_','_','X','_','_'},
+                {'_','_','X','_','_'},
+                {'_','X','_','_','_'},
+                {'_','_','X','_','_'},
+                {'_','_','X','_','_'}
+        };
+        boolean lose=isWin(notWin);
+        System.out.println("A board has a winner: "+lose);
+    }
+}
+
