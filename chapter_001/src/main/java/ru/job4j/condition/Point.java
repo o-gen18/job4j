8eef34f60207d076d1b036569e02e0e603643b6e
Ol Gen <oggen18@gmail.com>
2019-11-18 20:02:14 +0300

    4. Расстояние между точками в трехмерном пространстве.[#151258]

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -5,18 +5,35 @@ import static java.lang.Math.pow;
 
 public class Point {
     /**
-     * Это поля объекта. Они доступно только конкретному объекту
+     * Это поля объекта. Они доступно только конкретному объекту.(Экземпляры класса Point)
      */
     private int x;
     private int y;
+    private int z;
 
-    public Point(int first, int second){ //Создаётся конструктор для инициализации начального состояния
+    /**
+     * Создаётся конструктор для инициализации начального состояния объекта "точка"
+     * @param first координата "х"
+     * @param second координата "У"
+     */
+
+    public Point(int first, int second){
         this.x = first;
         this.y = second;
     }
 
+    public Point(int x, int y, int z){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
     public double distance(Point that) { // Метод принимает переменную типа Point, имеющую 2 параметра, приравниваемы соответственно к x и y
         return sqrt(pow(this.x-that.x, 2) + pow(this.y-that.y, 2));
+    }
+
+    public double distance3d(Point that) {
+        return sqrt(pow(this.x-that.x, 2) + pow(this.y-that.y, 2) + pow(this.z-that.z, 2));
     }
     public static void main(String[] args){
         Point a = new Point(0, 0);
--- chapter_001/src/test/java/ru/job4j/condition/PointTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -6,10 +6,20 @@ import org.junit.Test;
 
 public class PointTest {
     @Test
-    public void when5and5() {
-        double expected = 7.071;
-        double result = Point.distance(0, 0, 5, 5);
-        Assert.assertThat(result,closeTo(expected,0.001));
+   public void when5and5() {
+       double expected = 7.071;
+       Point a = new Point(0,0);
+       Point b = new Point(5,5);
+       double result = a.distance(b);
+       Assert.assertThat(result,closeTo(expected,0.001));
+    }
+    @Test
+    public void when5and5and5() {
+        double expected = 8.660;
+        Point a = new Point(0,0,0);
+        Point b = new Point(5,5,5);
+        double result = a.distance3d(b);
+        Assert.assertThat(result, closeTo(expected, 0.001));
     }
 }
 
