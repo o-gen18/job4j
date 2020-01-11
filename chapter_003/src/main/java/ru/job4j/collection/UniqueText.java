3c37f2e9e119eeb9ee54c5e5f75884ca250f30ad
Ol Gen <oggen18@gmail.com>
2020-01-11 18:54:10 +0300

    Идентичные тексты.

---
+++ chapter_003/src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,20 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class UniqueText {
+    public static boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = true;
+        String[] origin = originText.split(" ");
+        String[] duplicate = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+        for (String word : origin) {
+            check.add(word);
+        }
+        for (int i = 0; i < check.size(); i++) {
+            if (!(check.contains(duplicate[i]))) rsl = false; break;
+        }
+        return rsl;
+    }
+}
+
