0e1fa5e970a1130f8540cd65c138735a889a8fbe
Ol Gen <oggen18@gmail.com>
2020-01-28 20:19:40 +0300

    3. Компаратор для строк.[#151205]4

--- chapter_003/src/main/java/ru/job4j/collection/StringCompare.java
+++ chapter_003/src/main/java/ru/job4j/collection/StringCompare.java
@@ -5,39 +5,16 @@ import java.util.Comparator;
 public class StringCompare implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
-        int i = 0;
-        if (left.length() > right.length()) {
-            for (int index = 0; index < right.length(); index++) {
-                if (right.charAt(index) < left.charAt(index)) {
-                    i = 1;
-                    break;
-                } else if (right.charAt(index) > left.charAt(index)) {
-                    i = -1;
-                    break;
-                } else i = 1;
+        int result = left.length() - right.length();
+        int minLength = Math.min(left.length(), right.length());
+        for (int i = 0; i < minLength; i++) {
+            char c1 = left.charAt(i);
+            char c2 = right.charAt(i);
+            if (c1 != c2) {
+                result = c1 - c2;
+                break;
             }
-        } else if (left.length() < right.length()) {
-            for (int index = 0; index < left.length(); index++) {
-                if (left.charAt(index) > right.charAt(index)) {
-                    i = 1;
-                    break;
-                } else if (left.charAt(index) < right.charAt(index)) {
-                    i = -1;
-                    break;
-                } else i = -1;
-            }
-        } else {
-            for (int index = 0; index < left.length(); index++) {
-                    if (left.charAt(index) > right.charAt(index)) {
-                        i = 1;
-                        break;
-                    } else if (left.charAt(index) < right.charAt(index)) {
-                        i = -1;
-                        break;
-                    }
-            }
-
         }
-        return i;
+        return result;
     }
 }
