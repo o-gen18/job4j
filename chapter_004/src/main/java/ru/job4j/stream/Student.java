8dff9072426f866878347e8bcd4134703182a1e6
Ol Gen <oggen18@gmail.com>
2020-02-10 23:56:58 +0300

    1. Фильтрация учеников.[#151109]2

--- chapter_004/src/main/java/ru/job4j/stream/Student.java
+++ chapter_004/src/main/java/ru/job4j/stream/Student.java
@@ -1,5 +1,7 @@
 package ru.job4j.stream;
 
+import java.util.Objects;
+
 public class Student {
     private int score;
 
@@ -7,12 +9,24 @@ public class Student {
         this.score = score;
     }
 
-    public  int getScore() {
+    public int getScore() {
         return score;
     }
 
     @Override
     public String toString() {
         return "Student's score= " + score;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Student student = (Student) o;
+        return Objects.equals(score, student.score);
     }
 }
