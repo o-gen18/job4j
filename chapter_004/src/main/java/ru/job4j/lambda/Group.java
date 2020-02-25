f43e9b8ae9e6858f195f0a8bac566939e70c16c2
Ol Gen <oggen18@gmail.com>
2020-02-25 15:44:29 +0300

    Вопросы. Lambda, Stream API, JDK Features.[#151120] 2

--- chapter_004/src/main/java/ru/job4j/lambda/Group.java
+++ chapter_004/src/main/java/ru/job4j/lambda/Group.java
@@ -7,13 +7,7 @@ import java.util.stream.Collectors;
 public class Group {
     public static Map<String, Set<String>> sections(List<Student> students) {
         return students.stream()
-                .flatMap(st -> {
-                    List<Holder> holder = new ArrayList<>();
-                    for (String unit : st.getUnits()) {
-                        holder.add(new Holder(unit, st.getName()));
-                    }
-                    return holder.stream();
-                })
+                .flatMap(st -> st.getUnits().stream().map(section -> new Holder(section, st.getName())))
                 .collect(Collectors.groupingBy(ho -> ho.section,
                         Collector.of(HashSet::new, (set, el) -> set.add(el.nameOfStudent), (left, right) -> {
                         left.addAll(right); return left;
